3c41d85d5196be13e93a81cb0778b7cc2778a55f
= <bendev4977@gmail.com>
2024-05-30 20:27:33 +0200

    update

--- Views/partials/header.php
+++ Views/partials/header.php
@@ -1,6 +1,6 @@
 <header>
     <a role="link" class="link-h1" href="<?= $pathRedirect; ?>./">Free hub (dans free hub, aram rank, ect grissez non pro) new bug hex flash.. montrer extrait autre</a>
-    <a role="link" class="link-h1" href="<?= $pathRedirect; ?>./">Champions (petit tips, ect)</a>
+    <a role="link" class="link-h1" href="<?= $pathRedirect; ?>./">Champions (petit tips, ect) grizze avec ecrit pro</a>
 
     <a role="link" class="link-h1" href="<?= $pathRedirect; ?>./">Become Pro</a>
     <a role="link" class="link-h1" href="<?= $pathRedirect; ?>./">Login</a>
