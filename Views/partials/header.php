88c3c4d0e150258517c4304c840f1198052b2110
= <bendev4977@gmail.com>
2024-05-30 23:16:47 +0200

    update

--- Views/partials/header.php
+++ Views/partials/header.php
@@ -7,6 +7,7 @@
         </div>
 
         <ul role="list" class="flex">
+            <li role="listitem" class="m-r-15"><a role="link" class="link-menu" href="<?= $pathRedirect; ?>./">Home</a></li>
             <li role="listitem" class="m-r-15"><a role="link" class="link-menu" href="<?= $pathRedirect; ?>champions">Champions</a></li>
             <li role="listitem" class="m-r-15"><a role="link" class="link-menu" href="<?= $pathRedirect; ?>pro">Become Pro</a></li>
             <li role="listitem"><a role="link" class="link-menu" href="<?= $pathRedirect; ?>login">Login</a></li>
