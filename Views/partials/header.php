0d2d9bb97cc7539fe528940da08b41c2a62cac4f
= <bendev4977@gmail.com>
2024-05-30 22:13:30 +0200

    update

--- Views/partials/header.php
+++ Views/partials/header.php
@@ -2,12 +2,12 @@
     <nav role="navigation" aria-label="Main navigation" class="flex-between-center">
 
         <div>
-            <h1><a role="link" class="link-h1" href="<?= $pathRedirect; ?>./">WildRift Hub</a></h1>
+            <h1>WildRift Hub</h1>
         </div>
 
         <ul role="list" class="flex">
             <li role="listitem" class="m-r-15"><a role="link" class="link-menu" href="<?= $pathRedirect; ?>champions">Champions</a></li>
-            <li role="listitem"><a role="link" class="link-menu" href="<?= $pathRedirect; ?>pro">Become Pro</a></li>
+            <li role="listitem" class="m-r-15"><a role="link" class="link-menu" href="<?= $pathRedirect; ?>pro">Become Pro</a></li>
             <li role="listitem"><a role="link" class="link-menu" href="<?= $pathRedirect; ?>login">Login</a></li>
         </ul>
     </nav>
