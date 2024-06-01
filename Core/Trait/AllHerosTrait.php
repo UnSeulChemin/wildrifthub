339a7e29c063dac490ca70edcb64e90a9fa46402
= <bendev4977@gmail.com>
2024-06-01 04:55:05 +0200

    update

--- Core/Trait/AllHerosTrait.php
+++ Core/Trait/AllHerosTrait.php
@@ -7,5 +7,10 @@ Trait AllHerosTrait
      * all heros name to verificate param {hero}
      */
     private const ALL_HEROS =
-    ['aatrox', 'ahri', 'akali', 'alistar', 'amumu', 'annie', 'ashe', 'aurelionsol'];
+    ['aatrox', 'ahri', 'akali', 'alistar', 'amumu', 'annie', 'ashe', 'aurelionsol', 'akshan', 'blitzcrank', // 10
+        'brand', 'braum', 'caitlyn', 'camille', 'corki', 'darius', 'diana', 'drmundo', 'draven', 'ekko', // 10
+        'evelynn', 'ezreal', 'fiddlesticks', 'fiora', 'fizz', 'galio', 'garen', 'gragas', 'graves', 'gwen', // 10
+        'hecarim', 'irelia', 'janna', 'jarvaniv', 'jax', 'jayce', 'jhin', 'jinx', 'kaisa', 'kalista', // 10
+        'karma', 'kassadin', 'katarina', 'kayle', 'kayn', 'khazix', 'kindred', 'kennen', 'leesin', 'leona', // 10
+        'lillia', 'lucian', 'lulu', 'lux', 'malphite', 'masteryi', 'missfortune', 'morgana', 'nami', 'nasus']; // 10
 }
