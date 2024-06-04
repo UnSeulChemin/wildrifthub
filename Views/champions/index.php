1307998f01ee47c0e003642798040d9bfb9a9113
= <bendev4977@gmail.com>
2024-06-04 04:06:00 +0200

    update

--- Views/champions/index.php
+++ Views/champions/index.php
@@ -4,9 +4,9 @@
         <?php foreach($champions as $image): ?>
             <div class="div-card">
                <figure>
-                   <a role="link" class="flex" href="<?= $pathRedirect; ?>champions/champion/<?= $image->name ?>">
-                       <img alt="<?= $image->name?>" src="<?= $pathRedirect; ?>public/images/champions/<?= $image->image.".".$image->extension ?>">
-                   </a>
+                    <a role="link" class="flex" href="<?= $pathRedirect; ?>champions/champion/<?= $image->name ?>">
+                        <img alt="<?= $image->name?>" src="<?= $pathRedirect; ?>public/images/champions/<?= $image->image.".".$image->extension ?>">
+                    </a>
                </figure>
                <div class="flex-column-center-center">
                     <a class="link-card" href="<?= $pathRedirect; ?>champions/champion/<?= $image->name ?>"><?= ucfirst($image->name) ?></a>
