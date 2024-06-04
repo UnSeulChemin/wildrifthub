96b197c8b90d3d81c2c724b005ff326602c83c47
= <bendev4977@gmail.com>
2024-06-04 05:27:09 +0200

    update

--- Views/champions/champion.php
+++ Views/champions/champion.php
@@ -1,20 +1,43 @@
 <section class="section-content">
 
-<?php echo $champion->name ?>
+    <section class="section-card-name-head flex-around-center">
 
-    <figure class="figure-card">
-        <a role="link" class="flex" href="<?= $pathRedirect; ?>champions/name/<?= $champion->name ?>">
-            <img class="aa" alt="<?= $champion->name?>" src="<?= $pathRedirect; ?>public/images/champions/<?= $champion->image.".".$champion->extension ?>">
-        </a>
-    </figure>
+        <div>
+            <h3><?= ucfirst($champion->name) ?></h3>
+        </div>
 
-    <p>Role : <?php echo $champion->role ?></p>
+        <div>
+            <h3>Role</h3>
+            <h4><?= ucfirst($champion->role) ?></h4>
+        </div>
 
-    <h4>Basic tips</h4>
-    <p><?php echo $champion->free ?></p>
+        <div>
+            <h3>Difficulty</h3>   
+            <h4>***</h4> 
+        </div>
 
-    <h4>Pro tips</h4>
-    <p>u need to be pro.. click here (grissé?)</p>
+    </section>
+
+
+
+
+    <section class="section-card-id">
+
+            <figure class="figure-card-id">
+                <img alt="<?= $champion->name ?>"
+                    src="<?= $pathRedirect; ?>public/images/champions/<?= $champion->name.".".$champion->extension ?>">
+            </figure>
+
+            <div class="flex-center-center width-100">
+
+                <h4>Basic tips</h4>
+                <p><?php echo $champion->free ?></p>
+
+                <h4>Pro tips</h4>
+                <p>u need to be pro.. click here (grissé?)</p>
+
+            </div>
+    </section>
 
     <div class="m-t-30">
         <a class="link-section" href="javascript:history.go(-1)">Back</a>
