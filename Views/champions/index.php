745516e15a46af3bb0c9c87fd89adeb78bc27180
= <bendev4977@gmail.com>
2024-06-04 03:57:58 +0200

    update

--- Views/champions/index.php
+++ Views/champions/index.php
@@ -1,15 +1,18 @@
 <section class="section-content">
-   
+
    <section class="section-card">
-       <?php foreach($champions as $image): ?>
-           <div class="div-card">
-               <figure class="figure-card">
+        <?php foreach($champions as $image): ?>
+            <div class="div-card">
+               <figure>
                    <a role="link" class="flex" href="<?= $pathRedirect; ?>champions/champion/<?= $image->name ?>">
                        <img alt="<?= $image->name?>" src="<?= $pathRedirect; ?>public/images/champions/<?= $image->image.".".$image->extension ?>">
                    </a>
                </figure>
+               <div class="flex-column-center-center">
+                    <a class="link-card" href="<?= $pathRedirect; ?>champions/champion/<?= $image->name ?>"><?= ucfirst($image->name) ?></a>
+               </div>
            </div>
-       <?php endforeach; ?>
-   </section>
+        <?php endforeach; ?>
+    </section>
 
 </section>
--- Views/main/index.php
+++ Views/main/index.php
@@ -1,7 +1,6 @@
 <section class="section-content">
 
     <h2>what is wr hub..</h2>
-
     <p>winnerQ pgg.. montrer extrait autre</p>
 
 </section>
