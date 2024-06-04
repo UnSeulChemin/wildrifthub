50213c6a558523fd46d27d8dccdf33a6f88af961
= <bendev4977@gmail.com>
2024-06-04 06:29:30 +0200

    update

--- Views/champions/champion.php
+++ Views/champions/champion.php
@@ -18,26 +18,52 @@
 
     </section>
 
-
-
-
-    <section class="section-card-name">
-
-            <figure class="figure-card-id">
-                <img alt="<?= $champion->name ?>"
-                    src="<?= $pathRedirect; ?>public/images/champions/<?= $champion->name.".".$champion->extension ?>">
-            </figure>
-
+    <section class="section-card-name-image m-t-50">
+        <figure class="figure-card-id">
+            <img alt="<?= $champion->name ?>"
+            src="<?= $pathRedirect; ?>public/images/champions/<?= $champion->image.".".$champion->extension ?>">
+        </figure>
     </section>
 
 
-    <section class="section-card-name-content">
 
-        <h5>Basic tips for <span class="active"><?= ucfirst($champion->name) ?></span></h5>
-        <p><?php echo $champion->free ?></p>
+    <section class="section-card-name-image flex-around-center m-t-50">
 
-        <h5>Pro tips for <span class="active"><?= ucfirst($champion->name) ?></span></h5>
-        <p>u need to be pro.. click here (grissé?)</p>
+            <article class="flex-gap-50">
+
+                <div class="flex">
+                    <p>Core build</p>
+                    <p>luden</p>
+                    <p>orbe</p>
+                </div>
+
+                <div class="flex">
+                    <p>Rune</p>
+                    <p>luden</p>
+                    <p>orbe</p>
+                </div>
+
+                <div class="flex">
+                    <p>Spell</p>
+                    <p>luden</p>
+                    <p>orbe</p>
+                </div>
+
+            </article>
+
+    </section>
+
+    <section class="section-card-name-content m-t-50">
+
+        <article>
+            <h5>Basic tips for <span class="active"><?= ucfirst($champion->name) ?></span></h5>
+            <p><?= nl2br($champion->free) ?></p>
+        </article>
+
+        <article>
+            <h5>Pro tips for <span class="active"><?= ucfirst($champion->name) ?></span></h5>
+            <p>u need to be pro.. click here (grissé?)</p>
+        </article>
 
     </section>
 
