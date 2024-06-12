af8d2a96e620a52b6688257b500d569bec0d4889
= <bendev4977@gmail.com>
2024-06-12 20:12:44 +0200

    update

--- Core/Form.php
+++ Core/Form.php
@@ -5,11 +5,21 @@ class Form
 {
     private $formCode = "";
 
+    /**
+     * create
+     * @return void
+     */
     public function create()
     {
         return $this->formCode;
     }
 
+    /**
+     * validate
+     * @param array $form
+     * @param array $fields
+     * @return void
+     */
     public static function validate(array $form, array $fields)
     {
         foreach ($fields as $field)
@@ -22,6 +32,12 @@ class Form
         return true;
     }
 
+    /**
+     * validate email
+     * @param array $form
+     * @param array $emails
+     * @return void
+     */
     public static function validateEmail(array $form, array $emails)
     {
         foreach ($emails as $email)
@@ -34,6 +50,12 @@ class Form
         return true;
     }
 
+    /**
+     * validate password
+     * @param array $form
+     * @param array $passwords
+     * @return void
+     */
     public static function validatePassword(array $form, array $passwords)
     {
         foreach ($passwords as $password)
@@ -47,26 +69,6 @@ class Form
             }
         }
         return true;
-    }
-
-    private function addAttributes(array $attributes): string
-    {
-        $string = '';
-        $shorts = ['checked', 'disabled', 'readonly', 'multiple', 'required', 'autofocus', 'novalidate', 'formnovalidate'];
-
-        foreach ($attributes as $attribute => $value)
-        {
-            if (in_array($attribute, $shorts) && $value == true)
-            {
-                $string .= " $attribute";
-            }
-            
-            else
-            {
-                $string .= " $attribute=\"$value\"";
-            }
-        }
-        return $string;
     }
 
     public function startForm(string $method = 'post', string $action = '#', array $attributes = []): self
@@ -95,18 +97,18 @@ class Form
         return $this;
     }
 
+    public function addInput(string $type, string $name, array $attributes = []): self
+    {
+        $this->formCode .= "<input type='$type' name='$name'";
+        $this->formCode .= $attributes ? $this->addAttributes($attributes).'>' : '>';
+        return $this;
+    }
+
     public function addLabelFor(string $for, string $text, array $attributes = []): self
     {
         $this->formCode .= "<label for='$for'";
         $this->formCode .= $attributes ? $this->addAttributes($attributes) : '';
         $this->formCode .= ">$text</label>";
-        return $this;
-    }
-
-    public function addInput(string $type, string $name, array $attributes = []): self
-    {
-        $this->formCode .= "<input type='$type' name='$name'";
-        $this->formCode .= $attributes ? $this->addAttributes($attributes).'>' : '>';
         return $this;
     }
 
@@ -138,5 +140,25 @@ class Form
         $this->formCode .= $attributes ? $this->addAttributes($attributes) : '';
         $this->formCode .= ">$text</button>";
         return $this;
+    }
+
+    private function addAttributes(array $attributes): string
+    {
+        $string = '';
+        $shorts = ['checked', 'disabled', 'readonly', 'multiple', 'required', 'autofocus', 'novalidate', 'formnovalidate'];
+
+        foreach ($attributes as $attribute => $value)
+        {
+            if (in_array($attribute, $shorts) && $value == true)
+            {
+                $string .= " $attribute";
+            }
+            
+            else
+            {
+                $string .= " $attribute=\"$value\"";
+            }
+        }
+        return $string;
     }
 }
