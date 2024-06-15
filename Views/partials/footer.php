1d00a66291945446763af7b0641d0f45e477b1ef
= <bendev4977@gmail.com>
2024-06-15 22:37:49 +0200

    update

--- Views/partials/footer.php
+++ Views/partials/footer.php
@@ -1,4 +1,4 @@
 <footer>
-    <p class="color-white">© 2024-2024 WR.HUB. WR.HUB is not endorsed by Riot Games and does not reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends.</p>
-    <p class="color-white">League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</p>
+    <p class="p-white">© 2024-2024 WR.HUB. WR.HUB is not endorsed by Riot Games and does not reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends.</p>
+    <p class="p-white">League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</p>
 </footer>
