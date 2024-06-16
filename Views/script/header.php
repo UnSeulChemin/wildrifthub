8212a92e328132524e794771541b22fd17c207bc
= <bendev4977@gmail.com>
2024-06-16 22:51:15 +0200

    update

--- Views/script/header.php
+++ Views/script/header.php
@@ -4,4 +4,5 @@ $activeChampions = (str_contains($_GET['p'], 'champions')) ? 'active ' : null;
 $activeGuides = (str_contains($_GET['p'], 'guides')) ? 'active ' : null;
 $activePro = (in_array($_GET['p'], ['users'])) ? 'active ' : null;
 $activeLogin = (str_contains($_GET['p'], 'login')) ? 'active ' : null;
+$activeAdmin = (str_contains($_GET['p'], 'admin')) ? 'active ' : null;
 ?>
