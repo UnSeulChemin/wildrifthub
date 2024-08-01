92551dbb54bb95ed084e61361ca5d18c4a6b1f04
= <bendev4977@gmail.com>
2024-08-01 02:30:20 +0200

    update

--- Views/hub/index.php
+++ Views/hub/index.php
@@ -2,7 +2,7 @@
 
     <h2>Latest guides</h2>
 
-    <section class="flex-center-wrap-gap-35">
+    <section class="flex-center-wrap-gap-35 m-b-30">
         <?php foreach($guidesLatest as $image): ?>
             <div class="div-card">
                <figure class="figure-image">
@@ -17,21 +17,16 @@
         <?php endforeach; ?>
     </section>
 
-    <div class="m-tb-30">
-        <a class="link-section" href="guides"">See guides</a>
-    </div>
-
-    <div class="m-tb-30">
+    <div class="m-b-20 flex-center-center-gap-35">
+        <a class="link-section" href="guides"">Guides</a>
         <a class="link-section" href="guides/all"">All guides</a>
     </div>
 
     <h2>Latest champions</h2>
 
-    <div class="flex-center-center m-t-20">
+    <div class="flex-center-center-gap-35">
         <?php foreach($championsLatest as $image): ?>
-            <div>
-                <a class="link-card" href="champions/champion/<?= $image->name ?>"><?= ucfirst($image->name) ?></a>
-            </div>
+            <a class="link-section" href="champions/champion/<?= $image->name ?>"><?= ucfirst($image->name) ?></a>
         <?php endforeach; ?>
     </div>
 
