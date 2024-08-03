aa8f112c2aa1d6ab33589a074b8593137c7f0f7c
= <bendev4977@gmail.com>
2024-08-03 06:12:12 +0200

    update

--- Views/users/profile.php
+++ Views/users/profile.php
@@ -2,9 +2,14 @@
 
     <h2>Profile</h2>
    
-    <div>
+    <div class="m-b-40">
         <p class="m-b-20"><?= $user->email ?></p>
         <a role="link" class="link-profile" href="updateEmail">Update</a>
+    </div>
+
+    <div>
+        <p class="m-0">Compte crée le</p>
+        <p class="m-0"><?= date('d/m/Y', strtotime($user->created_at)); ?></p>
     </div>
 
 </section>
